344e4028e863d170dc78a38c8d207aff55d451be
David Cruz <david@codebycruz.com>
2026-02-05 01:06:35 -0800

    feat(hood,vk): device, command buffer, queue initial abstractions
    This should properly enumerate device queue family props to in the
    adapter to generate queues that have graphics capabilities which should
    work for everything we need (compute+rendering)
    
    Difficult thing to tackle now is getting a renderpass and framebuffer
    object. Framebuffer is easier, just create and hash on beginRendering
    descriptor. renderpass encapsulates other functionality so might want to
    make that outside of beginRendering, but not sure.

--- packages/hood/src/adapter/vk.lua
+++ packages/hood/src/adapter/vk.lua
@@ -4,12 +4,27 @@ local VKDevice = require("hood.device.vk")
 
 ---@class hood.vk.Adapter
 ---@field pd vk.PhysicalDevice
+---@field gfxQueueFamilyIdx number
 local VKAdapter = {}
 VKAdapter.__index = VKAdapter
 
 ---@param physicalDevice vk.PhysicalDevice
 function VKAdapter.new(physicalDevice)
-	return setmetatable({ pd = physicalDevice }, VKAdapter)
+	local queueFamilies = vk.getPhysicalDeviceQueueFamilyProperties(physicalDevice)
+
+	local gfxQueueFamilyIdx = nil
+	for i, family in ipairs(queueFamilies) do
+		if bit.band(family.queueFlags, vk.QueueFlagBits.GRAPHICS) ~= 0 then
+			gfxQueueFamilyIdx = i - 1 -- zero-based index
+			break
+		end
+	end
+
+	if not gfxQueueFamilyIdx then
+		error("No graphics queue family found")
+	end
+
+	return setmetatable({ pd = physicalDevice, gfxQueueFamilyIdx = gfxQueueFamilyIdx }, VKAdapter)
 end
 
 function VKAdapter:requestDevice()
--- packages/hood/src/device/vk.lua
+++ packages/hood/src/device/vk.lua
@@ -1,13 +1,27 @@
+local vk = require("arisu-vulkan")
+
 local VKBuffer = require("hood.buffer.vk")
+local VKQueue = require("hood.queue.vk")
 
 ---@class hood.vk.Device
+---@field public queue hood.vk.Queue
 ---@field device vk.Device
 local VKDevice = {}
 VKDevice.__index = VKDevice
 
----@param device vk.Device
-function VKDevice.new(device)
-	return setmetatable({ device = device }, VKDevice)
+---@param adapter hood.vk.Adapter
+function VKDevice.new(adapter)
+	local device = vk.createDevice(adapter.pd, {
+		queueCreateInfos = {
+			{
+				queueFamilyIndex = adapter.gfxQueueFamilyIdx,
+				queuePriorities = { 1.0 },
+			},
+		},
+	})
+	local queue = VKQueue.new(device, adapter.gfxQueueFamilyIdx, 0)
+
+	return setmetatable({ device = device, queue = queue }, VKDevice)
 end
 
 ---@param descriptor hood.BufferDescriptor
