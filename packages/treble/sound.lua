658b729f0a7d8cc5a2ac5c7c8672e023e603ddcf
David Cruz <david@codebycruz.com>
2026-02-05 14:42:11 -0800

    refactor: audio libraries renaming
    Put all of the sound playing libraries under the 'treble' name umbrella.
    
    The audio parsing library is now `apl`.

--- packages/treble/sound.lua
+++ packages/treble/sound.lua
@@ -3,6 +3,6 @@ local util = require("arisu-util")
 ---@class Sound
 ---@field play fun(audio: Audio, volume?: number): Sound
 local Sound = (
-	util.isLinux() and require("arisu-cpal.raw.alsa")
+	util.isLinux() and require("apl.raw.alsa")
 	or error("Unsupported OS for audio")
 ) --[[@as Sound]]
